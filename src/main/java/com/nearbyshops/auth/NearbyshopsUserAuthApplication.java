daa963ad35e610f9dc29989bf6e647a1ae1b6b0c
Diego-Murcia <dmurcia.cespedes@gmail.com>
2024-03-24 22:09:46 -0500

    feat: initialize project

---
+++ src/main/java/com/nearbyshops/auth/NearbyshopsUserAuthApplication.java
@@ -0,0 +1,13 @@
+package com.nearbyshops.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearbyshopsUserAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NearbyshopsUserAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nearbyshops/auth/NearbyshopsUserAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nearbyshops.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NearbyshopsUserAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
